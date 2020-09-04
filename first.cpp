e8f9e2f26a4c18251a2d2fe0e5be050d1ac4d9ba
chengliu <chengliu@upchina.com>
2020-09-04 10:22:29 +0800

    modify first.cpp

--- first.cpp
+++ first.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int  main(int argc, const char** argv) 
 {
     cout << "hello git,I'm coming ~~!" << endl;
-
+    
+    for (size_t i = 0; i < count; i++)
+    {
+        cout<<i<<endl;
+    }
     
     return 0;
 }
